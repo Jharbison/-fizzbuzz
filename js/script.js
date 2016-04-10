f4ce5488a5b77cd784b30a676b6169a1e7c10c7d
Jharbison <jeff.p.harbison@gmail.com>
2016-04-10 17:24:09 -0400

    FzBz update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-for (var i = 1; i <= 100; i++) {
+$(document).ready(function() {
+
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+
+		var inputNumber = $('.input-box').val()
+		var num = +inputNumber
+		$('.input-box').val('');
+		$('.fizzbuzz').empty();
+
+
+for (var i = 1; i <= inputNumber; i++) {
 	if (i%15===0) {
-		console.log("FizzBuzz")
+		$('.fizzbuzz').append('<li>FizzBuzz</li>');
 	}
 	else if (i%3===0) {
-		console.log("Fizz")
+		$('.fizzbuzz').append('<li>Fizz</li>');
 	}
 	else if (i%5===0) {
-		console.log("Buzz")
+		$('.fizzbuzz').append('<li>FizzBuzz</li>');
 	}
 	else {
-		console.log(i)
+		$('.fizzbuzz').append('<li>' + i + '</li>');
 	}
-}
+};
+
+});
+
+});
