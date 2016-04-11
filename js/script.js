0fa8e184615263dc2b8f9c95dd0f23a8064a4c3b
Jharbison <jeff.p.harbison@gmail.com>
2016-04-10 20:30:11 -0400

    FzBz

--- js/script.js
+++ js/script.js
@@ -1,37 +1,41 @@
 $(document).ready(function() {
 
-	$('form').on('submit', function(e) {
+	$('form').on('submit', function(event) {
 		event.preventDefault();
 
 		var inputNumber = $('.input-box').val()
 		var num = +inputNumber
-		$('.input-box').val('');
-		$('.fizzbuzz').empty();
+		clean();
+
+		if (num % 1 != 0 || num ===NaN || num >100 || num < 1) {
+		alert("Must use whole number");
+		} else { 
+			fizzBuzz(num);	
+		}
 
 
-for (var i = 1; i <= inputNumber; i++) {
-	if (i%15===0) {
-		$('.fizzbuzz').append('<li>FizzBuzz</li>');
-	}
-	else if (i%3===0) {
-		$('.fizzbuzz').append('<li>Fizz</li>');
-	}
-	else if (i%5===0) {
-		$('.fizzbuzz').append('<li>Buzz</li>');
-	}
-	else {
-		$('.fizzbuzz').append('<li>' + i + '</li>');
-	}
-};
-var clean = function () {
-		$('ul').remove();
-	}
+	});
 
-if (num % 1 != 0 && num !==NaN) {
-	clean();
-	alert("Must use whole number");
-}
+	var clean = function () {
+			$('.input-box').val('');
+			$('.fizzbuzz').empty();
+		}
 
-});
+	var fizzBuzz = function(inputNumber) {
+		for (var i = 1; i <= inputNumber; i++) {
+			if (i%15===0) {
+				$('.fizzbuzz').append('<li>FizzBuzz</li>');
+			}
+			else if (i%3===0) {
+				$('.fizzbuzz').append('<li>Fizz</li>');
+			}
+				else if (i%5===0) {
+				$('.fizzbuzz').append('<li>Buzz</li>');
+			}
+			else {
+				$('.fizzbuzz').append('<li>' + i + '</li>');
+			}
+		};
+	}
 
 });
